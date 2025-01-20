b834f627c7d2cf234960e52cc10357fa6161863e
Burak Yıldıran <106268716+burakyildirann@users.noreply.github.com>
2025-01-20 22:55:14 +0300

    Delete BOLUM8 directory

--- BOLUM8/B8/B8_1/B8_1_Dosyaya_Erisim.py
+++
@@ -1,8 +0,0 @@
-def dosyaAc(adres):
-    import os
-    if os.path.exists(adres):
-        return open(adres,"r+")
-    else:
-        return open(adres,"w")
-
-dosya = dosyaAc(r"Telefon\Defter.csv")
